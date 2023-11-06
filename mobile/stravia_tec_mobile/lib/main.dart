820714dd1368995a0ad52124ea6a34e29da522f6
Ignacio Calderón <53590021+Apollo13-0@users.noreply.github.com>
2023-11-06 15:55:47 -0600

    mapa funciona, timer no

--- mobile/stravia_tec_mobile/lib/main.dart
+++ mobile/stravia_tec_mobile/lib/main.dart
@@ -1,89 +1,152 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:xml/xml.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MapScreen(),
+      title: "StraviaTEC mobile",
+      home: MapPage(),
     );
   }
 }
 
-class MapScreen extends StatefulWidget {
+class MapPage extends StatefulWidget {
   @override
-  _MapScreenState createState() => _MapScreenState();
+  _MapPageState createState() => _MapPageState();
 }
 
-class _MapScreenState extends State<MapScreen> {
-  GoogleMapController mapController;
-  Location location = Location();
-  List<LatLng> routePoints = [];
+class _MapPageState extends State<MapPage> {
+  final Set<Marker> _markers = Set();
+  late GoogleMapController _controller;
+  LocationData? currentLocation;
+  int seconds = 0;
+  int minutes = 0;
+  late Timer timer;
 
   @override
   void initState() {
     super.initState();
-    location.onLocationChanged.listen((LocationData locationData) {
-      // Add the current location to the routePoints list
-      routePoints.add(LatLng(locationData.latitude, locationData.longitude));
-      // Update the map with the new location
-      mapController.animateCamera(CameraUpdate.newLatLng(LatLng(locationData.latitude, locationData.longitude));
-    });
+    getCurrentLocation();
   }
 
-  // Function to save the GPX file
-  void saveGPXFile() async {
-    final gpx = GpxDocument.create(
-      version: '1.1',
-      creator: 'Your App Name',
-      tracks: [
-        Track(
-          segments: [
-            TrackSegment(
-              points: routePoints.map((latLng) {
-                return TrackPoint(
-                  position: Position(latitude: latLng.latitude, longitude: latLng.longitude),
-                );
-              }).toList(),
-            ),
-          ],
-        ),
-      ],
-    );
+  void getCurrentLocation() async {
+    Location location = Location();
+    try {
+      currentLocation = await location.getLocation();
+      if (currentLocation != null) {
+        _addMarker(
+          LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+          'Ubicación Actual',
+        );
+      }
+    } catch (e) {
+      print('Error al obtener la ubicación: $e');
+    }
+  }
 
-    final gpxString = gpx.toString(pretty: true);
-
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/my_route.gpx');
-    await file.writeAsString(gpxString);
-
-    // You can also display a message to the user to indicate that the GPX file has been saved.
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Map Example'),
+        title: const Center(child: Text("Actividad")),
       ),
-      body: GoogleMap(
-        onMapCreated: (controller) {
-          mapController = controller;
-        },
-        initialCameraPosition: CameraPosition(target: LatLng(0.0, 0.0), zoom: 16.0),
-        myLocationEnabled: true,
-        mapType: MapType.normal,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: saveGPXFile,
-        child: Icon(Icons.save),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: currentLocation != null
+                  ? CameraPosition(
+                      target: LatLng(
+                        currentLocation!.latitude!,
+                        currentLocation!.longitude!,
+                      ),
+                      zoom: 15.0,
+                    )
+                  : CameraPosition(
+                      target: LatLng(37.7749, -122.4194),
+                      zoom: 15.0,
+                    ),
+              markers: _markers,
+              onMapCreated: (GoogleMapController controller) {
+                setState(() {
+                  _controller = controller;
+                });
+              },
+            ),
+          ),
+          Container(
+            color: Colors.amber,
+            child: Column(
+              children: [
+                Text(
+                  '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        startTimer();
+                      },
+                      child: Text("Iniciar"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        stopTimer();
+                      },
+                      child: Text("Detener"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void _addMarker(LatLng position, String markerId) {
+    final Marker marker = Marker(
+      markerId: MarkerId(markerId),
+      position: position,
+      infoWindow: InfoWindow(
+        title: markerId,
+      ),
+    );
+
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        seconds++;
+        if (seconds == 60) {
+          seconds = 0;
+          minutes++;
+        }
+      });
+    });
+  }
+
+  void stopTimer() {
+    timer.cancel();
   }
 }
