c550f4309fdad9126f9bf913de7708ed66d5cf49
Carlos12001 <carlos.andres12001@gmail.com>
2023-11-15 16:46:24 -0600

    fix: change header

--- web/StraviaTEC_Athlete/src/app/components/header/header.component.ts
+++ web/StraviaTEC_Athlete/src/app/components/header/header.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { User, user } from 'src/app/models/login.module';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  user: User = {
+    email: '',
+    password: '',
+  };
+
+  currentUrl: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.user = user;
+
+    let urlSegments = this.route.snapshot.url; // array of segments
+    this.currentUrl = urlSegments.map((segment) => segment.path).join('/');
+    console.log('Actual URL: ', this.currentUrl);
+  }
+}
