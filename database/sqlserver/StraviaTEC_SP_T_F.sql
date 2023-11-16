618148b8efe326c6cf7a0bc2484c8bb818668f1a
Carlos12001 <carlos.andres12001@gmail.com>
2023-11-15 19:23:46 -0600

    fix: sp

--- database/sqlserver/StraviaTEC_SP_T_F.sql
+++ database/sqlserver/StraviaTEC_SP_T_F.sql
@@ -1,12 +1,13 @@
-CREATE PROCEDURE sp_GetCountries
+CREATE OR ALTER PROCEDURE sp_GetCountries
 AS
 BEGIN
 	SELECT CountryName, Flag
 	FROM COUNTRY
 	ORDER BY CountryName
 END;
+GO
 
-CREATE PROCEDURE sp_OrganizerLogin @Email VARCHAR(25), @Password VARCHAR(16)
+CREATE OR ALTER PROCEDURE sp_OrganizerLogin @Email VARCHAR(25), @Password VARCHAR(16)
 AS
 BEGIN
 	DECLARE @Count INT;
@@ -17,8 +18,9 @@ BEGIN
 
     SELECT CASE WHEN @Count > 0 THEN 1 ELSE 0 END AS 'IsValid';
 END;
+GO
 
-CREATE PROCEDURE sp_AthleteLogin @Email VARCHAR(25), @Password VARCHAR(16)
+CREATE OR ALTER PROCEDURE sp_AthleteLogin @Email VARCHAR(25), @Password VARCHAR(16)
 AS
 BEGIN
 	DECLARE @Count INT;
@@ -29,8 +31,9 @@ BEGIN
 
     SELECT CASE WHEN @Count > 0 THEN 1 ELSE 0 END AS 'IsValid';
 END;
+GO
 
-CREATE PROCEDURE sp_NewAthlete @Aemail VARCHAR(25), 
+CREATE OR ALTER PROCEDURE sp_NewAthlete @Aemail VARCHAR(25), 
 							   @Apassword VARCHAR(16), 
 							   @Fname	VARCHAR(15), 
 							   @Mname	VARCHAR(15), 
@@ -44,19 +47,22 @@ BEGIN
 	INSERT INTO ATHLETE(Aemail, Apassword, Fname, Mname, Lname1, Lname2, Photo, Cno, Birth_date)
 	VALUES(@Aemail, @Apassword, @Fname, @Mname, @Lname1, @Lname2, @Photo, (SELECT Cnumber FROM COUNTRY WHERE CountryName = @CountryName), @Birth_date)
 END;
+GO
 
-CREATE PROCEDURE sp_GetAthletes
+CREATE OR ALTER PROCEDURE sp_GetAthletes
 AS
 BEGIN
 	SELECT Aemail, Apassword, Fname, Mname, Lname1, Lname2, Photo, Cno, CONVERT(VARCHAR(10), Birth_date, 120) AS Birth_date
 	FROM ATHLETE
 	ORDER BY Fname, Lname1
 END;
+GO
 
-CREATE PROCEDURE sp_GetAthleteByEmail @Email VARCHAR(25)
+CREATE OR ALTER PROCEDURE sp_GetAthleteByEmail @Email VARCHAR(25)
 AS
 BEGIN
 	SELECT Aemail, Apassword, Fname, Mname, Lname1, Lname2, Photo, Cno, CONVERT(VARCHAR(10), Birth_date, 120) AS Birth_date
 	FROM ATHLETE
 	WHERE Aemail = @Email
 END;
+GO
