4a46946b734a08cc2924152dcde35d99cfa3b488
Jose <jcur1802@gmail.com>
2023-11-16 17:22:12 -0600

    feat: get sports, post activities and follow/unfollow athletes
    theres also a fix in the DB

---
+++ api/api_sqlserver/StraviaTECAPISQLS/StraviaTECAPISQLS/Controllers/ChallengeController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using StraviaTECAPISQLS.Models;
+using System.Data;
+
+namespace StraviaTECAPISQLS.Controllers
+{
+    [Route("api/challenge")]
+    [ApiController]
+    public class ChallengeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ChallengeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public JsonResult Get()
+        {
+            string query = @"
+                EXEC sp_GetChallenges
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StraviaTEC");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        [Route("new")]
+        public async Task<JsonResult> Post(Challenge user)
+        {
+            string query = @"
+                 EXEC sp_NewChallenge @Cname, @Ctype, @StartDate, @FinalDate, @Pid, @SportName, @Mileage
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StraviaTEC");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Cname", user.Cname);
+                    myCommand.Parameters.AddWithValue("@Ctype", user.Ctype);
+                    DateOnly startDate = DateOnly.Parse(user.StartDate);
+                    myCommand.Parameters.AddWithValue("@StartDate", startDate);
+                    DateOnly finalDate = DateOnly.Parse(user.FinalDate);
+                    myCommand.Parameters.AddWithValue("@FinalDate", finalDate);
+                    myCommand.Parameters.AddWithValue("@Pid", user.Pid);
+                    myCommand.Parameters.AddWithValue("@SportName", user.SportName);
+                    myCommand.Parameters.AddWithValue("@Mileage", user.Mileage);   
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Reto añadido");
+        }
+
+    }
+}
--- database/sqlserver/StraviaTEC_SP_T_F.sql
+++ database/sqlserver/StraviaTEC_SP_T_F.sql
@@ -34,14 +34,14 @@ END;
 GO
 
 CREATE OR ALTER PROCEDURE sp_NewAthlete @Aemail VARCHAR(25), 
-							   @Apassword VARCHAR(16), 
-							   @Fname	VARCHAR(15), 
-							   @Mname	VARCHAR(15), 
-							   @Lname1	VARCHAR(15), 
-							   @Lname2 VARCHAR(15), 
-							   @Photo VARBINARY(MAX) = NULL, 
-							   @CountryName VARCHAR(30), 
-							   @Birth_date	DATE
+										@Apassword VARCHAR(16), 
+										@Fname	VARCHAR(15), 
+										@Mname	VARCHAR(15), 
+										@Lname1	VARCHAR(15), 
+									    @Lname2 VARCHAR(15), 
+										@Photo VARBINARY(MAX) = NULL, 
+										@CountryName VARCHAR(30), 
+										@Birth_date	DATE
 AS
 BEGIN
 	INSERT INTO ATHLETE(Aemail, Apassword, Fname, Mname, Lname1, Lname2, Photo, Cno, Birth_date)
@@ -66,3 +66,77 @@ BEGIN
 	WHERE A.Aemail = @Email
 END;
 GO
+
+CREATE OR ALTER PROCEDURE sp_GetSports
+AS
+BEGIN
+	SELECT SportName
+	FROM SPORT
+	ORDER BY SportName
+END;
+GO
+
+CREATE OR ALTER PROCEDURE sp_NewChallenge @Cname VARCHAR(50),
+										  @Ctype VARCHAR(8),
+										  @StartDate DATE,
+										  @FinalDate DATE,
+										  @Pid	INT,
+										  @SportName VARCHAR(10),
+									      @Mileage SMALLINT
+AS
+BEGIN
+	INSERT INTO CHALLENGE(Cname, Ctype, StartDate, FinalDate, Pid, Sptid, Mileage)
+	VALUES(@Cname, @Ctype, @StartDate, @FinalDate, @Pid, (SELECT SportID FROM SPORT WHERE SportName = @SportName), @Mileage)
+END;
+GO
+
+CREATE OR ALTER PROCEDURE sp_GetChallenges
+AS
+BEGIN
+	SELECT ChallengeID AS id , Cname AS challenge_name, Mileage AS 'description', Ctype AS challenge_type, StartDate AS 'start_date', FinalDate AS 'final_date' 
+	FROM CHALLENGE
+END;
+GO
+
+CREATE OR ALTER PROCEDURE sp_GetAthleteInformation @Aemail VARCHAR(25)
+AS
+BEGIN
+    SELECT A.Aemail,
+           CONCAT(A.Fname, ' ', A.Lname1) AS nombre,
+		   A.Fname AS firstname,
+		   ISNULL(A.Mname, '') AS secondname,
+		   A.Lname1 AS apellido1,
+		   A.Lname2 AS apellido2,
+		   C.CountryName AS pais,
+	       COUNT(DISTINCT AC.Challid) AS challenges,
+		   COUNT(DISTINCT I.Inumber) AS runs,
+		   CASE WHEN F.Afollows IS NOT NULL THEN 1 ELSE 0 END AS arefriends
+    FROM ATHLETE A
+    JOIN COUNTRY C ON A.Cno = C.Cnumber
+    LEFT JOIN ATHLETE_CHALLENGE AC ON A.Aemail = AC.Auser
+    LEFT JOIN INSCRIPTION I ON A.Aemail = I.Auser
+    LEFT JOIN FOLLOWS F ON @Aemail = F.Afollower AND F.Afollows = A.Aemail
+	WHERE A.Aemail <> @Aemail
+    GROUP BY A.Aemail, A.Fname, A.Mname, A.Lname1, A.Lname2, C.CountryName, F.Afollows
+	ORDER BY nombre;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE sp_FollowUnfollow @followerEmail VARCHAR(25), @followsEmail VARCHAR(25)
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM FOLLOWS WHERE Afollower = @followerEmail AND Afollows = @followsEmail)
+    BEGIN
+        DELETE FROM FOLLOWS 
+		WHERE Afollower = @followerEmail AND Afollows = @followsEmail;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO FOLLOWS (Afollower, Afollows) 
+		VALUES (@followerEmail, @followsEmail);
+    END
+
+	EXEC sp_GetAthleteInformation @followerEmail;
+END;
+GO
+
