b758fbe3eca926a9e2defd8b37ec3f009f6816ac
Jose <jcur1802@gmail.com>
2023-11-19 15:56:44 -0600

    feat: MongoDB API

---
+++ api/api_mongodb/StraviaTECAPIMongo/StraviaTECAPIMongo/Repositories/MongoDBRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace StraviaTECAPIMongo.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public MongoDBRepository() 
+        {
+            client = new MongoClient("mongodb://127.0.0.1:27017");
+
+            db = client.GetDatabase("Comments");
+        }
+    }
+}
